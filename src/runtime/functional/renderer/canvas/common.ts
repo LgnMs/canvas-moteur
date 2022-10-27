e094f254df9498288260e96eac78bd25d84f6c35
LiGuangNian <792841450@qq.com>
2022-10-27 17:44:54 +0800

    update

--- src/runtime/functional/renderer/canvas/common.ts
+++ src/runtime/functional/renderer/canvas/common.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
+import { CanvasComponent } from 'runtime/functional/project/component/canvas/canvasComponent';
 
-export abstract class ComponentRender<T> {
+export abstract class ComponentRender<T extends CanvasComponent> {
     component: T;
 
     constructor(component: T) {
@@ -9,6 +10,15 @@ export abstract class ComponentRender<T> {
     abstract toWebAxis(layerWidth: number, layerHeight: number): this;
     abstract getGeometry(): THREE.BufferGeometry;
     abstract getMaterial(): THREE.Material;
-    abstract update(): void;
     abstract parse(zIndex: number): THREE.Mesh;
+    
+    update() {
+        this.component.mesh?.geometry.dispose();
+        const geometry = this.getGeometry();
+        this.component.mesh!.geometry = geometry;
+
+        const color: THREE.Color = (this.component.mesh!.material as any).color;
+        color.setStyle(this.component.style.backgroundColor!);
+    }
+    
 }
