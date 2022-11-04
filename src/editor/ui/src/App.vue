e952a360f5eb50864d0990cb79cbaddcc553b886
LiGuangNian <792841450@qq.com>
2022-11-04 12:23:00 +0800

    add 系统字体堆栈

--- src/editor/ui/src/App.vue
+++ src/editor/ui/src/App.vue
@@ -8,5 +8,7 @@
 <style>
     body {
         margin: 0px;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial,
+            sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji';
     }
 </style>
