6ed8b1c861fe916fd0c27814352c48ddcd93973a
LiGuangNian <792841450@qq.com>
2022-11-04 14:42:59 +0800

    update

--- src/editor/ui/examples/demo1.ts
+++
@@ -1,8 +0,0 @@
-import { loadPorject } from "editor/common/loadProject";
-import { renderPage } from "editor/common/renderPage";
-
-export async function demo1(container: HTMLDivElement) {
-    const project = await loadPorject('/Users/liguang/Desktop/workSpace/MyProject/canvas-moteur/demo/project1/');
-
-    renderPage(container, project.getAllPages()[0])
-}
